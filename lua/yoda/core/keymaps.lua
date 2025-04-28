db5ea0b385d8af0cc3ecfce4b0a285133ba48cc3
Omar Crosby <omar.crosby@gmail.com>
2025-04-28 19:21:00 -0400

    chore: update smart buffer close

--- lua/yoda/core/keymaps.lua
+++ lua/yoda/core/keymaps.lua
@@ -9,8 +9,10 @@ vim.keymap.set("n", "<leader>e", function()
 
   if view.is_visible() then
     -- If you're already in the tree window → go right
-    if vim.api.nvim_get_current_win() == view.get_winnr() then
-      vim.cmd("wincmd l") -- move to the right window (your buffer)
+    local current_win = vim.api.nvim_get_current_win()
+    if current_win == view.get_winnr() then
+      api.tree.close() -- Close the tree if already focused
+      --vim.cmd("wincmd l") -- move to the right window (your buffer)
     else
       api.tree.focus() -- focus the tree
     end
@@ -18,6 +20,29 @@ vim.keymap.set("n", "<leader>e", function()
     api.tree.open() -- Tree isn't open? Open it
   end
 end, { desc = "Toggle focus between NvimTree and buffer" })
+
+
+local function smart_buffer_close()
+  local api = require("nvim-tree.api")
+
+  local curr_buf = vim.api.nvim_get_current_buf()
+
+  -- Try to close the buffer safely
+  vim.cmd("bdelete")
+
+  -- Get the new current buffer and window AFTER buffer deleted
+  local new_win = vim.api.nvim_get_current_win()
+  local new_buf = vim.api.nvim_win_get_buf(new_win)
+  local new_buf_name = vim.api.nvim_buf_get_name(new_buf)
+
+  -- If the new buffer is NvimTree, switch to next buffer
+  if new_buf_name:match("NvimTree_") then
+    vim.cmd("bnext")
+  end
+end
+
+-- Map <leader>q to close the current buffer
+vim.keymap.set("n", "<leader>q", smart_buffer_close, { desc = "Close Buffer (via Bufferline)" })
 
 
 vim.keymap.set("n", "<leader>ff", function()
@@ -36,6 +61,7 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to Left Window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to Lower Window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to Upper Window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to Right Window" })
+vim.keymap.set("n", "<C-c>", "<C-w>c", { desc = "Close window" })
 
 -- Visual Mode Improvements
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Selection Down" })
