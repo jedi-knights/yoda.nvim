aea0cc04226e086e7b0a0333eb8464c6b4f4ebaf
Omar Crosby <omar.crosby@weather.com>
2025-05-07 10:04:20 -0400

    chore: fix error message regarding async mason loading

--- lua/yoda/plugins/spec/lsp.lua
+++ lua/yoda/plugins/spec/lsp.lua
@@ -5,9 +5,12 @@ return {
   {
     "williamboman/mason.nvim",
     lazy = false,
-    build = ":MasonUpdate", -- Update installed servers on install
+    -- build = ":MasonUpdate", -- Temporarily removed to avoid async issues
     config = function()
-      require("mason").setup()
+      local mason_ok, mason = pcall(require, "mason")
+      if mason_ok then
+        mason.setup()
+      end
     end,
   },
 
@@ -16,14 +19,17 @@ return {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
     config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",   -- Lua
-          "pyright",  -- Python
-          "gopls",    -- Go
-          "ts_ls", -- JavaScript/TypeScript
-        },
-      })
+      local mason_lsp_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+      if mason_lsp_ok then
+        mason_lspconfig.setup({
+          ensure_installed = {
+            "lua_ls",   -- Lua
+            "pyright",  -- Python
+            "gopls",    -- Go
+            "ts_ls",    -- JavaScript/TypeScript (correct, future-safe)
+          },
+        })
+      end
     end,
   },
 
@@ -32,23 +38,24 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local lspconfig = require("lspconfig")
+      local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
       local lsp = require("yoda.lsp")
 
-      -- List of language servers
-      local servers = {
-        lua_ls = require("yoda.lsp.servers.lua_ls"),
-        pyright = require("yoda.lsp.servers.pyright"),
-        gopls = require("yoda.lsp.servers.gopls"),
-        eslint = require("yoda.lsp.servers.eslint"),
-        ts_ls = require("yoda.lsp.servers.ts_ls"),
-      }
+      if lspconfig_ok then
+        local servers = {
+          lua_ls = require("yoda.lsp.servers.lua_ls"),
+          pyright = require("yoda.lsp.servers.pyright"),
+          gopls = require("yoda.lsp.servers.gopls"),
+          eslint = require("yoda.lsp.servers.eslint"),
+          ts_ls = require("yoda.lsp.servers.ts_ls"), -- ✅ updated here too
+        }
 
-      for name, opts in pairs(servers) do
-        lspconfig[name].setup(vim.tbl_deep_extend("force", {
-          on_attach = lsp.on_attach,
-          capabilities = lsp.capabilities(),
-        }, opts))
+        for name, opts in pairs(servers) do
+          lspconfig[name].setup(vim.tbl_deep_extend("force", {
+            on_attach = lsp.on_attach,
+            capabilities = lsp.capabilities(),
+          }, opts))
+        end
       end
     end,
   },
@@ -58,11 +65,11 @@ return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
-      "hrsh7th/cmp-buffer",   -- Buffer completions
-      "hrsh7th/cmp-path",     -- Path completions
-      "saadparwaiz1/cmp_luasnip", -- Snippet completions
-      "L3MON4D3/LuaSnip",     -- Snippet engine
+      "hrsh7th/cmp-nvim-lsp",    -- LSP source for nvim-cmp
+      "hrsh7th/cmp-buffer",      -- Buffer completions
+      "hrsh7th/cmp-path",        -- Path completions
+      "saadparwaiz1/cmp_luasnip",-- Snippet completions
+      "L3MON4D3/LuaSnip",        -- Snippet engine
     },
     config = function()
       local cmp = require("cmp")
@@ -106,4 +113,3 @@ return {
     end,
   },
 }
-
