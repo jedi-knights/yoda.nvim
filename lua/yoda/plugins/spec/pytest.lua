875771b7db70f3743701a6bdf9194fbc4f22e7b2
Omar Crosby <omar.crosby@gmail.com>
2025-05-08 15:04:28 -0400

    chore: update neogit startup

--- lua/yoda/plugins/spec/pytest.lua
+++ lua/yoda/plugins/spec/pytest.lua
@@ -1,11 +1,16 @@
 return {
   {
     "jedi-knights/pytest.nvim",
-    dir = "~/src/github/jedi-knights/pytest.nvim",
+    dir = "~/src/github.com/jedi-knights/pytest.nvim",
     lazy = false,
     ft = "python",
     config = function()
-      require("pytest").setup()
+      local ok, pytest = pcall(require, "pytest")
+      if ok then
+        pytest.setup()
+      else
+        vim.notify("pytest.nvim failed to load", vim.log.levels.WARN)
+      end
     end,
   },
 }
