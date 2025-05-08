6d72e15be07780e869c33dffef318bf539a232fc
Omar Crosby <omar.crosby@gmail.com>
2025-05-08 15:04:28 -0400

    chore: update neogit startup

--- lua/yoda/plugins/spec/git.lua
+++ lua/yoda/plugins/spec/git.lua
@@ -19,6 +19,40 @@ return {
     cmd = { "Git", "G" },
   },
 
+
+  {
+    "NeogitOrg/neogit",
+    keys = { "<leader>gg" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",              -- optional
+      "echasnovski/mini.pick",         -- optional
+      "folke/snacks.nvim",             -- optional
+    },
+    config = function()
+      local neogit = require('neogit')
+      neogit.setup {}
+
+      -- Keybindings
+      local map = vim.keymap.set
+      local opts = { noremap = true, silent = true }
+
+      map('n', '<leader>gg', function() neogit.open() end, opts)
+      map('n', '<leader>gc', function() neogit.open({ 'commit' }) end, opts)
+      map('n', '<leader>gP', function() neogit.open({ 'push' }) end, opts)
+      map('n', '<leader>gp', function() neogit.open({ 'pull' }) end, opts)
+      map('n', '<leader>gl', function() neogit.open({ 'log' }) end, opts)
+      map('n', '<leader>gs', function() neogit.open({ 'status' }) end, opts)
+      map('n', '<leader>gb', ":Telescope git_branches<CR>", opts)
+      map('n', '<leader>gB', ":G blame<CR>", opts)
+      map('n', '<leader>gn', function() neogit.open({ 'branch' }) end, opts)
+    end,
+  },
+
   -- Git blame annotations (optional)
   -- {
   --   "f-person/git-blame.nvim",
