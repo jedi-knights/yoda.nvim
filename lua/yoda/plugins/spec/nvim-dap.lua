0791437db01424ca05219c07c0a3a899f8fbd4ce
Omar Crosby <omar.crosby@gmail.com>
2025-05-09 08:00:12 -0400

    chore: add debugging

--- lua/yoda/plugins/spec/nvim-dap.lua
+++ lua/yoda/plugins/spec/nvim-dap.lua
@@ -2,9 +2,17 @@
 return {
   {
     "mfussenegger/nvim-dap",
+    lazy = false,
     dependencies = {
-	  "nvim-neotest/nvim-nio",
       "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "nvim-telescope/telescope-dap.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-neotest/neotest",
+      "nvim-neotest/neotest-python",
+      "nvim-neotest/neotest-plenary",
+      "nvim-neotest/neotest-go",
+	    "nvim-neotest/nvim-nio",
       "mfussenegger/nvim-dap-python",
       "theHamsta/nvim-dap-virtual-text",
     },
@@ -41,6 +49,35 @@ return {
         numhl = "DiagnosticSignWarn",
       })
 
+      -- Eval var under cursor
+      vim.keymap.set("n", "<space>?", function()
+        require("dapui").eval(nil, { enter = true })
+      end)
+
+      --vim.keymap.set("n", "<F1>", dap.continue)
+      --vim.keymap.set("n", "<F2>", dap.set_into)
+      --vim.keymap.set("n", "<F3>", dap.step_over)
+      --vim.keymap.set("n", "<F4>", dap.step_out)
+      --vim.keymap.set("n", "<F5>", dap.step_back)
+      --vim.keymap.set("n", "<F6>", dap.restart)
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+
+
       -- Automatically open/close DAP UI
       dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
@@ -50,27 +87,27 @@ return {
 
       -- Toggle breakpoint
       vim.keymap.set("n", "<leader>db", function()
-        dap.toggle_breakpoint()
+        require('dap').toggle_breakpoint()
       end, opts)
 
       -- Continue / Start
       vim.keymap.set("n", "<leader>dc", function()
-        dap.continue()
+        require('dap').continue()
       end, opts)
 
       -- Step Over
       vim.keymap.set("n", "<leader>do", function()
-        dap.step_over()
+        require('dap').step_over()
       end, opts)
 
       -- Step Into
       vim.keymap.set("n", "<leader>di", function()
-        dap.step_into()
+        require('dap').step_into()
       end, opts)
 
       -- Step Out
       vim.keymap.set("n", "<leader>dO", function()
-        dap.step_out()
+        require('dap').step_out()
       end, opts)
 
       -- Keymap to terminate debugging
