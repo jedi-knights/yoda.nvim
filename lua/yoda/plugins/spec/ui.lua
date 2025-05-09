f908d0b5cd772273d60e2789718aeb4319cd91d5
Omar Crosby <omar.crosby@weather.com>
2025-05-09 09:43:31 -0400

    chore(neotree): display .github and .gitignore files in neotree

--- lua/yoda/plugins/spec/ui.lua
+++ lua/yoda/plugins/spec/ui.lua
@@ -34,8 +34,6 @@ return {
           hide_dotfiles = true,
           hide_gitignored = true,
           hide_by_name = {
-            ".github",
-            ".gitignore",
             "package-lock.json",
             ".changeset",
             ".prettierrc.json",
