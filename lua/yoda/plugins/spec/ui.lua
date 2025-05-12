58ae087de6327713167cb354efeaed5b8bdedbc8
Omar Crosby <omar.crosby@gmail.com>
2025-05-12 05:39:50 -0400

    chore: add a file type trigger for render-markdown

--- lua/yoda/plugins/spec/ui.lua
+++ lua/yoda/plugins/spec/ui.lua
@@ -125,12 +125,15 @@ return {
   -- Markdown preview
   {
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    ft = { 'markdown' },
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    opts = {
+      render_modes = { 'n', 'c', 't' }, -- render modes to use
+    },
   },
 }
 
