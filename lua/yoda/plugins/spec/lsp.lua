70e6fb6a61c807c44540bdf3accd6d7b1afbbd19
Omar Crosby <omar.crosby@weather.com>
2025-05-12 06:31:12 -0400

    chore: add eslint as a requirement to lsp

--- lua/yoda/plugins/spec/lsp.lua
+++ lua/yoda/plugins/spec/lsp.lua
@@ -27,6 +27,7 @@ return {
             "pyright",  -- Python
             "gopls",    -- Go
             "ts_ls",    -- JavaScript/TypeScript (correct, future-safe)
+            "eslint",   -- JavaScript/TypeScript (correct, future-safe)
           },
         })
       end
