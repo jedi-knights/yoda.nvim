cfd7c8cd3c5fd3f0801b585354eae9f149a4660d
Omar Crosby <omar.crosby@weather.com>
2025-05-13 14:15:09 -0400

    chore: update pytest config

--- lua/yoda/plugins/spec/pytest.lua
+++ lua/yoda/plugins/spec/pytest.lua
@@ -1,22 +1,24 @@
 return {
-  {
-    "jedi-knights/pytest.nvim",
-    dir = "~/src/github.com/jedi-knights/pytest.nvim",
-    lazy = false,
-    ft = "python",
+  "jedi-knights/pytest.nvim",
+  dir = "~/src/github.com/jedi-knights/pytest.nvim",
+  lazy = false,
+  ft = "python",
 
-    cond = function()
-      return require("pytest.detect").should_load_plugin()
-    end,
+  cond = function()
+    return require("pytest.detect").should_load_plugin()
+  end,
 
-    config = function()
-      local ok, pytest = pcall(require, "pytest")
-      if ok then
-        pytest.setup()
-      else
-        vim.notify("pytest.nvim failed to load", vim.log.levels.WARN)
-      end
-    end,
-  },
+  config = function()
+    local ok, pytest = pcall(require, "pytest")
+    if ok then
+      pytest.setup()
+    else
+      vim.notify("pytest.nvim failed to load", vim.log.levels.WARN)
+    end
+
+    vim.keymap.set("n", "<leader>pt", function()
+      require("pytest.commands").start()
+    end, { desc = "Run pytest" })
+  end,
 }
 
