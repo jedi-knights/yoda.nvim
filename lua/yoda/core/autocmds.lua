4fdcee30421b025b25ae535acb734c809238d54e
Omar Crosby <omar.crosby@gmail.com>
2025-05-14 21:36:03 -0400

    chore: experiment with neovim terminal

--- lua/yoda/core/autocmds.lua
+++ lua/yoda/core/autocmds.lua
@@ -3,6 +3,15 @@
 -- Helper function to define autocommands more easily
 local autocmd = vim.api.nvim_create_autocmd
 
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('yoda-terminal', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
--- lua/yoda/core/keymaps.lua
+++ lua/yoda/core/keymaps.lua
@@ -2,7 +2,25 @@
 
 -- General Keymaps
 
--- Toggle relative numbers
+-- Start a small terminal at the bottom of the screen
+--
+local job_id = 0
+vim.keymap.set("n", "<leader>st", function ()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 5)
+
+  job_id = vim.opt.channel
+end)
+
+vim.keymap.set("n", "<leader>example", function()
+  -- make
+  -- go build, go test, go run something (examples)
+  vim.fn.chansend(job_id, { "echo 'hi'\r\n" })
+end, { desc = "Send command to terminal" })
+
+-- Toggle relative numbersq
 vim.keymap.set("n", "<leader>r", ":set relativenumber!<CR>", { desc = "Toggle Relative Line Numbers" })
 
 -- Toggle neo-tree
