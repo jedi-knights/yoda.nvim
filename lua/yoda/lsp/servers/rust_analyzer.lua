2fe416a53a5901f9f691eff54c557f91693bbb50
Omar Crosby <omar.crosby@gmail.com>
2025-06-05 06:11:46 -0400

    chore: add rust_analyzer LSP server config

---
+++ lua/yoda/lsp/servers/rust_analyzer.lua
@@ -0,0 +1,13 @@
+-- lua/yoda/lsp/servers/rust_analyzer.lua
+return {
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      },
+      checkOnSave = {
+        command = "clippy",
+      },
+    },
+  },
+}
