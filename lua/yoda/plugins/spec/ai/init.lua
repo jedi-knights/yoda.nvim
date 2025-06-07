1481bcd765a3ecf6a8feb96d181172aca51c5026
Omar Crosby <omar.crosby@gmail.com>
2025-06-07 03:57:18 -0400

    refactor: reorganize plugin specs

---
+++ lua/yoda/plugins/spec/ai/init.lua
@@ -0,0 +1,6 @@
+-- lua/yoda/plugins/spec/ai/init.lua
+
+return {
+    require("yoda.plugins.spec.ai.copilot"),
+}
+
---
+++ lua/yoda/plugins/spec/obsidian/init.lua
@@ -0,0 +1,6 @@
+-- lua/yoda/plugins/spec/obsidian
+
+return {
+    require("yoda.plugins.spec.obsidian.obsidian"),
+}
+
