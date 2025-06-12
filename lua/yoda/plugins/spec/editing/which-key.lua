aab5390b68ed6695255d51a00071f3bd9ad4ff2a
Omar Crosby <omar.crosby@weather.com>
2025-06-12 04:37:57 -0400

    chore: Update which-key.lua

--- lua/yoda/plugins/spec/editing/which-key.lua
+++ lua/yoda/plugins/spec/editing/which-key.lua
@@ -1,19 +1,12 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section in the documentation
-  },
+  opts = {},
   keys = {
     {
       "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
+      "<cmd>WhichKey<CR>",
+      desc = "Open which-key",
     },
   },
 }
-
