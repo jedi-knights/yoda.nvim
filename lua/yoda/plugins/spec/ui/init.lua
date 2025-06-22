38f2a38e357f7212ebc33519b63b985aab16b490
Omar Crosby <omar.crosby@gmail.com>
2025-06-22 10:54:02 -0400

    chore: remove other stuff covered by mini

--- lua/yoda/plugins/spec/ui/init.lua
+++ lua/yoda/plugins/spec/ui/init.lua
@@ -1,9 +1,6 @@
 -- lua/yoda/plugins/spec/ui
 
 return {
-    require("yoda.plugins.spec.ui.bufferline"),
-    require("yoda.plugins.spec.ui.indent-blankline"),
-    require("yoda.plugins.spec.ui.lualine"),
     require("yoda.plugins.spec.ui.showkeys"),
     require("yoda.plugins.spec.ui.snacks"),
     require("yoda.plugins.spec.ui.tokyonight"),
