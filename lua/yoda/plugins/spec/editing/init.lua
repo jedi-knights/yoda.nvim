ca3562628c5874c1bd486480242f94280604cf38
Omar Crosby <omar.crosby@gmail.com>
2025-06-22 10:31:25 -0400

    chore: remove redundant plugins & migrate to mini

--- lua/yoda/plugins/spec/editing/init.lua
+++ lua/yoda/plugins/spec/editing/init.lua
@@ -1,10 +1,6 @@
 -- lua/yoda/plugins/spec/editing
 
 return {
-    require("yoda.plugins.spec.editing.comment"),
     require("yoda.plugins.spec.editing.mini"),
-    require("yoda.plugins.spec.editing.nvim-autopairs"),
-    require("yoda.plugins.spec.editing.nvim-surround"),
-    require("yoda.plugins.spec.editing.which-key"),
 }
 
