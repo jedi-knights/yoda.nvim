9a763cfb17806d8c3da5259f96097ef48e8e4be1
Omar Crosby <omar.crosby@weather.com>
2025-07-17 10:02:50 -0400

    chore: update ai setup

--- lua/yoda/plugins/spec/ai/init.lua
+++ lua/yoda/plugins/spec/ai/init.lua
@@ -1,7 +1,6 @@
 -- lua/yoda/plugins/spec/ai/init.lua
 
 return {
-    require("yoda.plugins.spec.ai.chatgpt"),
     require("yoda.plugins.spec.ai.copilot"),
     require("yoda.plugins.spec.ai.mcphub"),
     require("yoda.plugins.spec.ai.avante"),
