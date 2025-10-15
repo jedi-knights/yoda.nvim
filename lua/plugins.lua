a203304dad9bd10339a88b967265c01b75269827
Omar Crosby <omar.crosby@weather.com>
2025-10-15 10:47:03 -0400

    chore: add a status line to show my branch

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -228,6 +228,44 @@ return {
     end,
   },
 
+  -- Lualine - Statusline with git branch display
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "tokyonight",
+          component_separators = { left = "|", right = "|" },
+          section_separators = { left = "", right = "" },
+          globalstatus = true,
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = {
+            "branch", -- Shows current git branch
+            "diff",
+            "diagnostics",
+          },
+          lualine_c = { "filename" },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { "filename" },
+          lualine_x = { "location" },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        extensions = { "nvim-tree", "trouble", "aerial" },
+      })
+    end,
+  },
+
   -- ============================================================================
   -- LSP & COMPLETION
   -- ============================================================================
