cef890e3b25f8140e446bdbe70c4cc5852889ed4
Omar Crosby <omar.crosby@gmail.com>
2025-10-15 05:53:12 -0400

    chore: update rust LSP setup

--- lua/options.lua
+++ lua/options.lua
@@ -31,6 +31,7 @@ vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.inccommand = "split"
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
+vim.opt.colorcolumn = "80"
 
 -- ============================================================================
 -- INDENTATION
