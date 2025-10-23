3f513c397bf03adbf0458c05e58097548d601ba3
Omar Crosby <omar.crosby@weather.com>
2025-10-23 08:32:48 -0400

    feat(ui): enable Snacks input module with rounded border

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -77,6 +77,12 @@ return {
         terminal = {
           enabled = true,
         },
+        input = {
+          enabled = true,
+          win = {
+            border = "rounded",
+          },
+        },
       })
     end,
   },
