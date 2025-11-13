08b5ea963d59f2d89d296169ce2fdc6e795f5c27
Omar Crosby <omar.crosby@weather.com>
2025-11-13 09:02:23 -0500

    feat(editor): add Gherkin syntax highlighting support for feature files

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,89 @@
 -- lua/plugins/editor.lua
 -- editor plugins
 
-return {}
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function()
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.gherkin = {
+        install_info = {
+          url = "https://github.com/binhtran432k/tree-sitter-gherkin",
+          files = { "src/parser.c", "src/scanner.c" },
+          branch = "main",
+        },
+        filetype = "feature",
+      }
+
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "lua",
+          "vim",
+          "vimdoc",
+          "python",
+          "rust",
+          "go",
+          "javascript",
+          "typescript",
+          "c_sharp",
+          "json",
+          "yaml",
+          "toml",
+          "markdown",
+          "markdown_inline",
+          "bash",
+          "regex",
+          "gherkin",
+        },
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+          },
+        },
+      })
+
+      vim.treesitter.language.register("gherkin", "feature")
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "feature",
+        callback = function()
+          vim.bo.commentstring = "# %s"
+        end,
+      })
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    lazy = true,
+  },
+}
