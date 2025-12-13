08a5567b818573b5a70438b303996727f43271c6
Omar Crosby <omar.crosby@weather.com>
2025-12-13 10:53:36 -0500

    fix: improve snacks explorer tracking to prevent focus keymap failures
    - Fix close matcher to use exact filetype match instead of incorrect pattern
    - Replace excessive cache invalidation events with targeted buffer tracking
    - Add BufEnter autocmd to detect and handle buffer changes in explorer window
    - Add buffer validation to prevent invalid cache entries
    - Add <leader>ed debug command to inspect explorer state
    - Improves reliability of <leader>ef (focus explorer) keymap

--- lua/yoda/keymaps/explorer.lua
+++ lua/yoda/keymaps/explorer.lua
@@ -7,24 +7,71 @@ end
 
 local snacks_explorer_cache = {}
 
+local function invalidate_cache()
+  snacks_explorer_cache = {}
+end
+
 vim.api.nvim_create_autocmd("WinClosed", {
   callback = function(ev)
     local win_id = tonumber(ev.match)
     if snacks_explorer_cache.win == win_id then
-      snacks_explorer_cache = {}
+      invalidate_cache()
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  callback = function(ev)
+    if snacks_explorer_cache.buf and ev.buf == snacks_explorer_cache.buf then
+      invalidate_cache()
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function(ev)
+    if snacks_explorer_cache.buf and ev.buf == snacks_explorer_cache.buf then
+      invalidate_cache()
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  callback = function(ev)
+    if snacks_explorer_cache.win and vim.api.nvim_win_is_valid(snacks_explorer_cache.win) then
+      local current_buf = vim.api.nvim_win_get_buf(snacks_explorer_cache.win)
+      if current_buf ~= snacks_explorer_cache.buf then
+        local ft = vim.bo[current_buf].filetype
+        if ft ~= "snacks-explorer" then
+          invalidate_cache()
+        else
+          snacks_explorer_cache.buf = current_buf
+        end
+      end
     end
   end,
 })
 
 local function get_snacks_explorer_win()
   if snacks_explorer_cache.win and vim.api.nvim_win_is_valid(snacks_explorer_cache.win) then
-    return snacks_explorer_cache.win, snacks_explorer_cache.buf
+    local buf = vim.api.nvim_win_get_buf(snacks_explorer_cache.win)
+    if vim.api.nvim_buf_is_valid(buf) then
+      local ft = vim.bo[buf].filetype
+      if ft == "snacks-explorer" and snacks_explorer_cache.buf == buf then
+        return snacks_explorer_cache.win, buf
+      else
+        invalidate_cache()
+      end
+    else
+      invalidate_cache()
+    end
   end
 
   local win_utils = require("yoda-window.utils")
   local win, buf = win_utils.find_snacks_explorer()
 
-  if win then
+  if win and vim.api.nvim_buf_is_valid(buf) then
     snacks_explorer_cache = { win = win, buf = buf }
     return win, buf
   end
@@ -61,7 +108,7 @@ map("n", "<leader>ec", function()
   if win then
     local win_utils = require("yoda-window.utils")
     local count = win_utils.close_windows(function(win, buf, buf_name, ft)
-      return ft:match("snacks_") or ft == "snacks"
+      return ft == "snacks-explorer"
     end, true)
   else
     notify.notify("Snacks Explorer is not open", "info")
@@ -86,6 +133,7 @@ map("n", "<leader>e?", function()
     "<leader>ef - Focus explorer",
     "<leader>er - Refresh explorer",
     "<leader>ec - Close explorer",
+    "<leader>ed - Debug explorer state",
     "",
     "In Explorer:",
     "H - Toggle hidden files",
@@ -98,3 +146,35 @@ map("n", "<leader>e?", function()
 
   notify.notify(table.concat(help_text, "\n"), "info", { title = "Snacks Explorer Help" })
 end, { desc = "Explorer: Show help" })
+
+map("n", "<leader>ed", function()
+  local win_utils = require("yoda-window.utils")
+  local found_win, found_buf = win_utils.find_snacks_explorer()
+  
+  local debug_info = {
+    "Explorer Debug Info:",
+    "",
+    "Cached state:",
+    "  win: " .. tostring(snacks_explorer_cache.win),
+    "  buf: " .. tostring(snacks_explorer_cache.buf),
+    "",
+    "Search results:",
+    "  found_win: " .. tostring(found_win),
+    "  found_buf: " .. tostring(found_buf),
+    "",
+  }
+  
+  if found_win then
+    local buf = vim.api.nvim_win_get_buf(found_win)
+    local ft = vim.bo[buf].filetype
+    local buf_name = vim.api.nvim_buf_get_name(buf)
+    table.insert(debug_info, "Found window details:")
+    table.insert(debug_info, "  buffer: " .. buf)
+    table.insert(debug_info, "  filetype: " .. ft)
+    table.insert(debug_info, "  buf_name: " .. buf_name)
+  else
+    table.insert(debug_info, "No explorer window found")
+  end
+  
+  notify.notify(table.concat(debug_info, "\n"), "info", { title = "Explorer Debug" })
+end, { desc = "Explorer: Debug state" })
