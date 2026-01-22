1890a22a6042227d310f5045232e8beecc05e7dd
Omar Crosby <omar.crosby@weather.com>
2026-01-22 08:49:07 -0500

    chore: remove C# language support

--- lua/yoda/commands/dev_setup/init.lua
+++ lua/yoda/commands/dev_setup/init.lua
@@ -7,7 +7,6 @@ function M.setup()
   require("yoda.commands.dev_setup.rust").setup()
   require("yoda.commands.dev_setup.python").setup()
   require("yoda.commands.dev_setup.javascript").setup()
-  require("yoda.commands.dev_setup.csharp").setup()
 end
 
 return M
