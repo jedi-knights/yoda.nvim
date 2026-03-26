d544f5ea292a96e2f4bf987f969f4c499d730b79
Omar Crosby <omar.crosby@weather.com>
2026-03-25 20:52:55 -0400

    chore: remove dead plugin specs for neotest-python and nvim-treesitter-textobjects

--- lua/plugins/neotest-python.lua
+++
@@ -1,7 +0,0 @@
--- lua/plugins/neotest-python.lua
-
-return {
-  "nvim-neotest/neotest-python",
-  lazy = true,
-  dependencies = { "nvim-neotest/neotest" },
-}
--- lua/plugins/nvim-treesitter-textobjects.lua
+++
@@ -1,6 +0,0 @@
--- lua/plugins/nvim-treesitter-textobjects.lua
-
-return {
-  "nvim-treesitter/nvim-treesitter-textobjects",
-  lazy = true,
-}
