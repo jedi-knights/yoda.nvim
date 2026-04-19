ac6c49654292e3ac712bd5e54cc2e3a6710ff7f9
Omar Crosby <omar.crosby@gmail.com>
2026-04-19 09:58:10 -0400

    chore: update the window keymaps and lazy-lock.lua file

--- lua/yoda/keymaps/window.lua
+++ lua/yoda/keymaps/window.lua
@@ -3,6 +3,11 @@ local function map(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, opts)
 end
 
+map("n", "<C-h>", "<C-w>h", { desc = "Window: Move left" })
+map("n", "<C-j>", "<C-w>j", { desc = "Window: Move down" })
+map("n", "<C-k>", "<C-w>k", { desc = "Window: Move up" })
+map("n", "<C-l>", "<C-w>l", { desc = "Window: Move right" })
+
 map("n", "<leader>|", ":vsplit<cr>", { desc = "Window: Vertical split - Split window vertically" })
 map("n", "<leader>-", ":split<cr>", { desc = "Window: Horizontal split - Split window horizontally" })
 map("n", "<leader>ws", "<c-w>=", { desc = "Window: Equalize sizes - Make all splits equal size" })
